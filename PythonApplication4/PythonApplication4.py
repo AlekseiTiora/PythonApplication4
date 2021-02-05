93144b2b85f8bb0d6a224ede7fbc949b8456bdbd
opilane <opilane@HP-CZC6297BBM>
2021-02-05 11:59:40 +0200

    f

--- PythonApplication4/PythonApplication4.py
+++ PythonApplication4/PythonApplication4.py
@@ -5,92 +5,62 @@ from tkinter.filedialog import *
 import sys,fileinput
 from tkinter.messagebox import *
 root=Tk()
-root.geometry("400x300")
+root.geometry("1200x720")
 root.title("Описание машин")
 
 tabs=ttk.Notebook(root)
 
-texts=["Esimene","Teine","Kolmas","Neljas"]
 
 
 
 def image_A6():
     global img
-    img=PhotoImage(file="A6.png").subsample(2)
+    img=PhotoImage(file="A6.png").subsample(1)
     can.create_image(10,10,image=img,anchor=NW)
-    btn=Button(root, font="Arial 13", bd=5,text="Описание")
-    btn.pack()
+
 
 def image_rsq3():
     global img
-    img=PhotoImage(file="rsq3.png").subsample(8)
+    img=PhotoImage(file="rsq3.png").subsample(4)
     can.create_image(10,10,image=img,anchor=NW)
-    btn=Button(root, font="Arial 12", bd=5,text="Описание")
-    btn.pack()
+    
 
-def image_Q5():
-    global img
-    img=PhotoImage(file="Q5.png").subsample(4)
-    can.create_image(10,10,image=img,anchor=NW)
-    btn=Button(root, font="Arial 13", bd=5,text="Описание")
-    btn.pack()
-
-def image_A7():
-    global img
-    img=PhotoImage(file="A7.png").subsample(4)
-    can.create_image(10,10,image=img,anchor=NW)
-    btn=Button(root, font="Arial 13", bd=5,text="Описание")
-    btn.pack()
 
 def image_f90():
     global img
-    img=PhotoImage(file="f90.png").subsample(7)
+    img=PhotoImage(file="f90.png").subsample(4)
     can.create_image(10,10,image=img,anchor=NW)
-    btn=Button(root, font="Arial 13", bd=5,text="Описание")
-    btn.pack()
 
 def image_e60():
     global img
-    img=PhotoImage(file="e60.png").subsample(3)
+    img=PhotoImage(file="e60.png").subsample(2)
     can.create_image(10,10,image=img,anchor=NW)
-    btn=Button(root, font="Arial 13", bd=5,text="Описание")
-    btn.pack()
 
-def image_m4():
+def image_bmw():
     global img
-    img=PhotoImage(file="m4.png").subsample(4)
-    can.create_image(10,10,image=img,anchor=NW)
-    btn=Button(root, font="Arial 13", bd=5,text="Описание")
-    btn.pack()
+    img=PhotoImage(file="bmw.png").subsample (1)
+    can.create_image(10,15,image=img,anchor=NW)
 
-def image_e():
+def image_bmw1():
     global img
-    img=PhotoImage(file="e.png").subsample(4)
-    can.create_image(10,10,image=img,anchor=NW)
-    btn=Button(root, font="Arial 13", bd=5,text="Описание")
-    btn.pack()
+    img=PhotoImage(file="bmw1.png").subsample (1)
+    can.create_image(10,15,image=img,anchor=NW)
 
-def image_c():
+def image_audi():
     global img
+    img=PhotoImage(file="audi.png").subsample (1)
+    can.create_image(10,15,image=img,anchor=NW)
 
-    img=PhotoImage(file="c.png").subsample(5)
-    can.create_image(10,10,image=img,anchor=NW)
-    btn=Button(root, font="Arial 13", bd=5,text="Описание")
-    btn.pack()
-
-def image_s():
+def image_audi1():
     global img
-
-    img=PhotoImage(file="s.png").subsample(2)
-    can.create_image(10,10,image=img,anchor=NW)
-    btn=Button(root, font="Arial 13", bd=5,text="Описание")
-    btn.pack()
+    img=PhotoImage(file="audi1.png").subsample (1)
+    can.create_image(10,15,image=img,anchor=NW)
 
 
- 
-#for i in range(4): #len(text)
-#    tab1=Frame(tabs)
-#    tabs.add(tab1,text=texts[i])
+
+
+
+
 tab1=Frame(tabs)
 tabs.add(tab1,text="Автомобили")
 tabs.pack()
@@ -101,29 +71,31 @@ m1=Menu(M,tearoff=1)
 M.add_cascade(label="Audi",menu=m1)
 m1.add_command(label="A6",command=lambda:image_A6())
 m1.add_command(label="RSQ3",command=lambda:image_rsq3())
-m1.add_command(label="Q5",command=lambda:image_Q5())
-m1.add_command(label="A7",command=lambda:image_A7())
-m1.add_separator()
+
+
 
 
 m2=Menu(M,tearoff=2)
 M.add_cascade(label="Bmw",menu=m2)
 m2.add_command(label="M5F90",command=lambda:image_f90())
 m2.add_command(label="E60",command=lambda:image_e60())
-m2.add_command(label="M4",command=lambda:image_m4())
+
+m3=Menu(M,tearoff=2)
+M.add_cascade(label="Характеристики",menu=m3)
+m3.add_command(label="M5F90",command=lambda:image_bmw())
+m3.add_command(label="E60",command=lambda:image_bmw1())
+m3.add_command(label="audi A6",command=lambda:image_audi())
+m3.add_command(label="Audi rsq3",command=lambda:image_audi1())
 
 
 
-can=Canvas(tab1,width=300,height=200)
+
+
+can=Canvas(tab1,width=1200,height=720)
 can.pack()
 
-m3=Menu(M,tearoff=3)
-M.add_cascade(label="Mercedes",menu=m3)
-m3.add_command(label="e-class",command=lambda:image_e())
-m3.add_command(label="c-class",command=lambda:image_c())
-m3.add_command(label="s-class",command=lambda:image_s())
 
-btn=Button
+
 
 
 
